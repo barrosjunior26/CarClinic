3f9cefa006b233c9b91589290c59dbbbacdd4b06
Diniz Ribeiro <84927857+barrosjunior26@users.noreply.github.com>
2024-10-26 22:13:39 -0300

    Inclusão dos métodos assíncronos Entrar e Sair, como os próprios nomes dos métodos informam, um será responsável for fazer a configuração de login do usuário no portal, e o outro será responsável por fazer o processo de saída (logOut) do portal. Foi realizado também a configuração de conexão com o bando de dados do SqlServer no arquivo .json e a configuração no Program.cs

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarClinic.Data;
+using CarClinic.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
 namespace CarClinic.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly ApplicationDbContext _banco;
+        public LoginController(ApplicationDbContext db)
+        {
+            _banco = db;
+        }
+
         [HttpGet]
         public IActionResult Entrar()
         {
@@ -18,5 +27,31 @@ namespace CarClinic.Controllers
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Entrar(string Email, string Senha)
+        {
+            LoginModel login = _banco.tb_login.Where(x => x.Email == Email && x.Senha == Senha).FirstOrDefault();
+
+            if (login == null)
+            {
+                TempData["ErroValidadeLogin"] = "E-mail ou senha está incorreto, por favor, tente novamente. Se o problema persistir, entre em contato com o suporte.";
+                return View();
+            }
+
+            var claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Name, login.Email));
+            claims.Add(new Claim(ClaimTypes.Sid, login.Senha.ToString()));
+
+            var identidade = new ClaimsIdentity(claims, "Aceso");
+            ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identidade);
+            await HttpContext.SignInAsync("CookieAuthentication", claimsPrincipal, new AuthenticationProperties());
+            return Redirect("/Portal/Index");
+        }
+
+        public async Task<IActionResult> Sair()
+        {
+            await HttpContext.SignOutAsync();
+            return View(nameof(Entrar));
+        }
     }
 }
